a09fed91943325d6e9d821ce6cb49ad9ee076e9f
TJ <tjbuffington@yahoo.com>
2024-02-26 15:28:37 -0500

    Make Controllers for POST and GET

--- IssuesSolution/IssuesApi/Controllers/DemoController.cs
+++ IssuesSolution/IssuesApi/Controllers/DemoController.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace IssuesApi.Controllers;
+﻿namespace IssuesApi.Controllers;
 
 
 /// <summary>
@@ -18,9 +16,94 @@ public class DemoController : Controller //Extends the Controller Class from Asp
 
     //If a GET request is sent to the below address, then do this...
     [HttpGet("/demo")]
-    public ActionResult GetTheDemo()
+    public ActionResult<DemoResponse> GetTheDemo()
     {
-        return Ok();
+        //Create an instance of the DemoResponse object we want to send for this Request
+        var response = new DemoResponse("Request was Successful", DateTimeOffset.Now);
+        return Ok(response);
     }
 
+    [HttpGet("/employees")]
+    public ActionResult GetEmployees([FromQuery] string department = "ALL")
+    {
+        //Create an instance of the EmployeeResponse object we want to send for this Request
+        var employees = new List<EmployeeResponse>
+        {
+            new EmployeeResponse(Guid.NewGuid(), "Hans Soletick", "Contractor"),
+            new EmployeeResponse(Guid.NewGuid(), "Meg Ricardobluff", "DEV"),
+            new EmployeeResponse(Guid.NewGuid(), "Dr. Mantis Toboggan", "Medical"),
+            new EmployeeResponse(Guid.NewGuid(), "Lou Dorchen", "Laundry")
+        };
+
+        if (department == "ALL")
+        {
+            var response = new CollectionResponse<EmployeeResponse>(employees);
+            return Ok(response);
+
+        }
+        else
+        {
+            var filteredEmployees = employees.Where(e => e.Department == department).ToList();
+            var response = new CollectionResponse<EmployeeResponse>(employees);
+            return Ok(response);
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="employeeId">Directly links to the parameter being passed through the GET</param>
+    /// <returns></returns>
+
+    [HttpGet("/employees/{employeeId}")]
+    public ActionResult GetAnEmployee(int employeeId)
+    {
+        //Create an instance of the EmployeeResponse object we want to send for this Request
+        var employees = new List<EmployeeResponse>
+        {
+            new EmployeeResponse(Guid.NewGuid(), "Hans Soletick", "Contractor"),
+            new EmployeeResponse(Guid.NewGuid(), "Meg Ricardobluff", "DEV"),
+            new EmployeeResponse(Guid.NewGuid(), "Dr. Mantis Toboggan", "Medical"),
+            new EmployeeResponse(Guid.NewGuid(), "Lou Dorchen", "Laundry")
+        };
+
+        if (employeeId % 2 == 0)
+        {
+            //var filteredEmployees = employees.Where(e => e.Id == employeeId).ToList();
+            //var response = new CollectionResponse<EmployeeResponse>(employees);
+            return Ok(new EmployeeResponse(Guid.NewGuid(), "Hans Soletick", "Contractor"));
+
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
 }
+
+
+[HttpPost("/employees")]
+public ActionResult HireAnEmployee([FromBody] EmployeeHireRequest request)
+{
+
+
+}
+
+
+
+
+
+
+
+
+/// <summary>
+/// Creating a public record of the Response to send 
+/// </summary>
+/// <param name="message"></param>
+/// <param name="createdAt"></param>
+public record DemoResponse(string message, DateTimeOffset createdAt);
+
+public record EmployeeResponse(Guid Id, string Name, string Department);
+
+public record CollectionResponse<T>(List<T> Data);
+public record EmployeeHireRequest(string Name, string Department);
